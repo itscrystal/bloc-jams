4970391ec74e6fc051fc345d9e92f207074f576a
itsCrystal <tong.crystal@gmail.com>
2017-08-04 22:13:47 -0700

    checkpoint 31 buttons: completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // Yet Another Example Album
+ var albumAltJ = {
+     title: 'An Awesome Wave',
+     artist: 'alt-J',
+     label: 'Iguana Studio',
+     year: '2012',
+     albumArtUrl: 'assets/images/album_covers/22.png',
+     songs: [
+         { title: 'Intro', duration: '2:37' },
+         { title: 'Interlude 1', duration: '1:12' },
+         { title: 'Tassellate', duration: '3:01'},
+         { title: 'Breezeblocks', duration: '3:47' },
+         { title: 'Interlude 2', duration: '1:17'}
+     ]
+ };
